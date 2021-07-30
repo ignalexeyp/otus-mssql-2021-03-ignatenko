2742a5de1f9aa892d3a7fb61276d797078dc01f7
ignalexeyp <alexey_igo@mail.ru>
2021-07-30 16:41:14 +0300

    Update HW30.sql

--- W30_PARTITION/HW30.sql
+++ W30_PARTITION/HW30.sql
@@ -65,3 +65,17 @@ select COUNT(*) from [Warehouse].[StockItemTransactions];
 
 select COUNT(*) from [Warehouse].[StockItemTransactionsPart];
 
+select distinct t.name
+from sys.partitions p
+inner join sys.tables t
+	on p.object_id = t.object_id
+where p.partition_number <> 1;
+
+
+SELECT  $PARTITION.fnYearPartition(TransactionOccurredWhen) AS Partition
+		, COUNT(*) AS [COUNT]
+		, MIN(TransactionOccurredWhen)
+		,MAX(TransactionOccurredWhen) 
+FROM [Warehouse].[StockItemTransactionsPart]
+GROUP BY $PARTITION.fnYearPartition(TransactionOccurredWhen) 
+ORDER BY Partition ;  
